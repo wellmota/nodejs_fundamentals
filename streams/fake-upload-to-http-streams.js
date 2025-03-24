2adf6d92aa45875e40b7ad6bcc89548b730a9050
wellmota <impeto.wellington@gmail.com>
2025-03-24 17:53:44 -0300

    add OneToHundredStream and InverseNumberStream classes for stream processing and server handling

---
+++ streams/fake-upload-to-http-streams.js
@@ -0,0 +1,28 @@
+import {Readable} from 'node:stream'
+
+
+
+class OneToHundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+
+        this.push(buf)
+      }
+    }, 100)
+  }
+}
+
+
+fetch('http://localhost:3334', {
+  method: 'POST',
+  body: new OneToHundredStream(),
+  duplex: 'half'
+})
