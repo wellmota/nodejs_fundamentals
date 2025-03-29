e82a3ad3a3401b068a4fc750c7cf46da3b2087eb
wellmota <impeto.wellington@gmail.com>
2025-03-29 17:03:21 -0300

    implement in-memory database for user management and update server logic

--- src/server.js
+++ src/server.js
@@ -1,19 +1,8 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
-// Create a server that listens on port 3333
-
-// GET /users - List all users
-// POST /users - Create a new user
-// PUT /users/:id - Update a user
-// PATCH /users/:id - Update a user
-// DELETE /users/:id - Delete a user
-
-// Cabeçalhos ( requisição e resposta) => Metadados
-
-// HTTP Status Code
-
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request
@@ -21,19 +10,20 @@ const server = http.createServer(async (request, response) => {
   await json(request, response)
 
   if (method === 'GET' && url === '/users') {
-    return response
-      .setHeader('Content-Type', 'application/json')
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+    return response.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = request.body
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
     return response.writeHead(201).end()
   }
 
