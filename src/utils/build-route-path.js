6b994c2fc322dafbd83fe453136ffa0196149886
wellmota <impeto.wellington@gmail.com>
2025-03-30 09:41:03 -0300

    refactor buildRoutePath utility to use Array.from for route parameter matching

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,5 +1,5 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-z]+)/g
-  console.log(path.matchAll(routeParametersRegex))
+  console.log(Array.from(path.matchAll(routeParametersRegex)))
   return new RegExp()
 }
